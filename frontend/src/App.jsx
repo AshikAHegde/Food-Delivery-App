6434263d3fbdcd24312103f3605f69e88784d4be
AshikAHegde <hegdeashik13@gmail.com>
2025-06-25 19:36:45 +0530

    Initial Frontend HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import {
   Link
 } from "react-router-dom";
 import Login from './screens/login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
   return (
@@ -21,3 +24,5 @@ function App() {
 }
 
 export default App;
+
+//38min
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,18 +5,18 @@ import {
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+                <Link className="navbar-brand fs-1 fs-italic" to="#">GoFood</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/login">Login</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
                         </li>
 
                     </ul>
